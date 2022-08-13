152d05590b372f0b8612e5eb33702116b7ac5ba1
Muhammad Rais <101076504+mraism@users.noreply.github.com>
2022-08-13 23:33:39 +0700

    Update and rename oddEven to oddEven.js

---
+++ oddEven.js
@@ -0,0 +1,11 @@
+function oddEven(nilai){
+    if (nilai % 2 == 0) {
+        console.log('Nilai '+ nilai +" adalah Bilangan Genap");
+    }
+    else {
+        console.log('Nilai '+ nilai +" adalah Bilangan Ganjil");
+    }
+    return nilai;
+}
+
+oddEven(82), oddEven(81)
